feced617e28388262514543e6a390c2db676a46e
WictorHiago <wictor.hsc@gmail.com>
2022-04-27 19:00:13 -0400

    retornando numero menor,meio, maior

--- teste.js
+++ teste.js
@@ -11,14 +11,32 @@ function menorMeioMaior(A,B,C) {
             meio = B
             menor = C
         }else{
+            //se não for...
             meio = C
             menor = B
         }
-    }else if
-        (A < B && A < C) {
+    }else if (A < B && A < C) {
             menor = A
+            //verificar entre B e C
+        if(B > C) {
+            maior = B
+            menor = C
+        }else{
+            //se não for...
+            maior = C
+            menor = B
+        }
     }else{
         meio = A
+        //verificar entre B e C
+        if(B > C) {
+            maior = B
+            menor = C
+        }else{
+            //se não for...
+            maior = C
+            menor = B
+        }
     }
 
     console.log('menor', menor)
@@ -26,4 +44,4 @@ function menorMeioMaior(A,B,C) {
     console.log('maior', maior)
 
 }
-console.log(menorMeioMaior(500,100,900))
+console.log(menorMeioMaior(30,40,20))
